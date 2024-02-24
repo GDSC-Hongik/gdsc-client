d7bdbd7930f36020984edfce2a5406ea805deeb2
nimod7890 <nimod1234@g.skku.edu>
2024-02-25 06:03:54 +0900

    feat: update guide link

--- src/routes/routePath.ts
+++ src/routes/routePath.ts
@@ -9,11 +9,15 @@ const RoutePath = {
   AuthenticationProcess2_StudentVerification: '/student-verification',
   AuthenticationProcess3_Signup: '/signup',
 
-  GitHubGuideLink: '#',
-  StudentEmailLinkGuideLink: '#',
+  GitHubGuideLink:
+    'https://childlike-kumquat-540.notion.site/GitHub-0b7f5fa12fd84bd39c0a1ba732af04da?pvs=4',
+  StudentEmailLinkGuideLink:
+    'https://childlike-kumquat-540.notion.site/af30789ee9f940ab95cb19ffe18f2e44?pvs=74',
 
-  Terms: '#',
-  PersonalPrivacy: '#'
+  Terms:
+    'https://childlike-kumquat-540.notion.site/GDSC-Bevy-64588c93dbc147ebbd01fe7971af07df?pvs=4',
+  PersonalPrivacy:
+    'https://childlike-kumquat-540.notion.site/af30789ee9f940ab95cb19ffe18f2e44?pvs=74'
 } as const;
 
 export default RoutePath;
