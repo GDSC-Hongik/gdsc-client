58f49b7a9672df9677a095ec0d938f123d7ef0d7
강나연 <seoultownroad@naver.com>
2024-02-25 02:23:04 +0900

    feat: GDSCStatus -> BevyStatus로 변경, 마이페이지 UI 완료

--- src/components/myPage/BevyStatus.tsx
+++ src/components/myPage/BevyStatus.tsx
@@ -3,39 +3,39 @@ import { Flex, Text } from '@/components/common/Wrapper';
 import { theme } from '@/styles';
 import styled from '@emotion/styled';
 
-export type GDSCStatusType = 'PENDING' | 'VERIFIED';
+export type BevyStatusType = 'PENDING' | 'VERIFIED';
 
-export const GDSCStatus = ({ gdscStatus }: { gdscStatus: GDSCStatusType }) => {
+export const BevyStatus = ({ bevyStatus }: { bevyStatus: BevyStatusType }) => {
   return (
-    <Wrapper gdscStatus={gdscStatus} gap={12}>
+    <Wrapper bevyStatus={bevyStatus} gap={12}>
       <Flex direction="column" align="flex-start" gap={12}>
         <Text typo="label1" color="black">
-          {gdscStatus === 'PENDING'
+          {bevyStatus === 'PENDING'
             ? 'GDSC Bevy 가입이 마무리되지 않았어요.'
             : 'GDSC Bevy 가입이 완료되었어요.'}
         </Text>
         <Text typo="body2" color="gray4">
-          {gdscStatus === 'PENDING'
+          {bevyStatus === 'PENDING'
             ? '전체 GDSC 이벤트를 확인할 수 있는 플랫폼이에요. 지금 가입해볼까요? '
             : '전 세계 GDSC에서 어떤 이벤트가 진행되고 있는지 직접 확인해보실 수 있어요.'}
         </Text>
       </Flex>
       {/* TODO 디스코드 연동 페이지로 라우팅 */}
-      {gdscStatus === 'PENDING' && <RightArrow onClick={() => {}} />}
+      {bevyStatus === 'PENDING' && <RightArrow onClick={() => {}} />}
     </Wrapper>
   );
 };
 
 const Wrapper = styled(Flex)<{
-  gdscStatus: GDSCStatusType;
+  bevyStatus: BevyStatusType;
 }>`
   padding: 24px 24px 20px 24px;
   box-sizing: border-box;
 
   background-color: ${theme.palette.white};
   border-radius: 8px;
-  border: ${({ gdscStatus }) =>
-    gdscStatus === 'PENDING'
+  border: ${({ bevyStatus }) =>
+    bevyStatus === 'PENDING'
       ? `1px solid ${theme.palette.red100}`
       : `1px solid ${theme.palette.blue100}`};
 `;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,3 +1,92 @@
+import { Flex, Space, Text } from '@/components/common/Wrapper';
+import { media, theme } from '@/styles';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { ProgressBar } from '@/components/myPage/ProgressBar';
+import { PaymentStatus } from '@/components/myPage/PaymentStatus';
+import { DiscordStatus } from '@/components/myPage/DiscordStatus';
+import { BevyStatus } from '@/components/myPage/BevyStatus';
+import { Privacy } from '@/components/myPage/Privacy';
+
+// TODO
+// 1. 헤더 넣기
+// 2. 유저 정보 하드코딩 된 부분 수정
+// 3. 각 버튼 onClick 이벤트 핸들러 등록
 export const MyPage = () => {
-  return <div>my page</div>;
+  return (
+    <Wrapper direction="column" justify="flex-start">
+      {/* Header */}
+      <Space height={67} />
+      <Space height={35} />
+      <Text typo="heading3" color="black">
+        강나연 님
+      </Text>
+      <Space height={12} />
+      <Text
+        onClick={() => {}}
+        typo="label2"
+        color="gray4"
+        css={css`
+          text-decoration: underline;
+          cursor: pointer;
+        `}>
+        로그아웃
+      </Text>
+      <Space height={24} />
+      <Flex justify="flex-start" direction="column" align="flex-start">
+        <Text typo="heading4" color="black">
+          신청 상태
+        </Text>
+        <Space height={19} />
+        <ProgressBar currentStatus="PENDING" />
+        <Space height={48} />
+        <Text typo="heading4" color="black">
+          가입 조건
+        </Text>
+        <Space height={15} />
+        <PaymentStatus paymentStatus="PENDING" depositorName="김홍익" />
+        <Space height={12} />
+        <PaymentStatus paymentStatus="VERIFIED" depositorName="김홍익" />
+        <Space height={12} />
+        <DiscordStatus discordStatus="PENDING" />
+        <Space height={12} />
+        <DiscordStatus discordStatus="VERIFIED" />
+        <Space height={12} />
+        <BevyStatus bevyStatus="VERIFIED" />
+        <Space height={12} />
+        <BevyStatus bevyStatus="PENDING" />
+        <Space height={48} />
+        <Text typo="heading4" color="black">
+          가입 정보
+        </Text>
+        <Space height={12} />
+        <Text typo="body1" color="gray4">
+          이미 제출한 기존 정보를 수정해야 할 경우,
+          <br />
+          GDSC 채널톡으로 문의 주세요.
+        </Text>
+        <Space height={15} />
+        <Privacy
+          studentId="C011001"
+          department="컴퓨터공학과"
+          phone="01075546670"
+          email="nay35n@gmail.com"
+        />
+      </Flex>
+      <Space height={104} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled(Flex)`
+  min-height: 100vh;
+  width: 390px;
+  margin: 0px -16px;
+  padding: 0px 16px;
+
+  background-color: ${theme.palette.gray1};
+
+  ${media.mobile} {
+    width: 100vw;
+  }
+`;
