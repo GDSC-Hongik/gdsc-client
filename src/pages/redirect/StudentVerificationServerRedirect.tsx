61f4513fc45ac77316aa3121b8aae43e0644c4d9
nimod7890 <nimod1234@g.skku.edu>
2024-02-26 23:37:08 +0900

    feat: 재학생 인증 서버 리다이렉트 페이지 수정

--- src/pages/redirect/StudentVerificationServerRedirect.tsx
+++ src/pages/redirect/StudentVerificationServerRedirect.tsx
@@ -1,59 +1,63 @@
-import { Flex, Text } from '@/components/common/Wrapper';
+import { Text } from '@/components/common/Wrapper';
 import { useVerifyStudent } from '@/hooks/query';
+import { theme } from '@/styles';
 import styled from '@emotion/styled';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { PulseLoader } from 'react-spinners';
-import useLandingStatus from '@/hooks/zustand/useLandingStatus';
-import { getAuthRedirectPath } from '@/utils/auth';
-import { Button } from '@/components/common/Button';
 
 export const StudentVerificationServerRedirect = () => {
   const [searchParams] = useSearchParams();
-  const navigation = useNavigate();
+
   const token = searchParams.get('token');
-  const { error, isPending } = useVerifyStudent(token);
-
-  const { landingStatus } = useLandingStatus();
-
-  const message = (() => {
-    if (!token) {
-      return '이메일 인증 토큰이 존재하지 않습니다.';
-    }
-
-    if (error) {
-      return '이메일 인증 토큰이 유효하지 않습니다.';
-    }
-
-    return '잘못된 접근입니다.';
-  })();
+  const { isSuccess, isPending } = useVerifyStudent(token);
 
   return (
     <Container>
-      {(() => {
-        if (isPending) {
-          return <PulseLoader loading={isPending} />;
-        }
-
-        return (
-          <InfoContainer>
-            <Text color="red100">{message}</Text>
-            <Button
-              onClick={() => navigation(getAuthRedirectPath(landingStatus))}>
-              다시 로그인하기
-            </Button>
-          </InfoContainer>
-        );
-      })()}
+      <Box>
+        {isPending ? (
+          <PulseLoader loading={isPending} />
+        ) : (
+          <TextContainer>
+            <Text typo={'heading3'} style={{ marginBottom: '12px' }}>
+              {isSuccess ? '재학생 인증 완료' : '인증에 실패했어요'}
+            </Text>
+            {isSuccess ? (
+              <>
+                <Text>원래 페이지로 돌아가</Text>
+                <Text>인증하기 버튼을 눌러주세요.</Text>
+              </>
+            ) : (
+              <>
+                <Text>유효하지 않거나 만료된 인증코드예요.</Text>
+                <Text>원래 페이지로 돌아가서</Text>
+                <Text>메일 재전송 버튼을 눌러주세요.</Text>
+              </>
+            )}
+          </TextContainer>
+        )}
+      </Box>
     </Container>
   );
 };
 
-const Container = styled(Flex)`
+const Container = styled.div`
   width: 100%;
-  padding: 100px 16px;
+  padding: 40px 16px;
 `;
 
-const InfoContainer = styled(Flex)`
+const Box = styled.div`
+  display: flex;
   flex-direction: column;
+  background-color: ${theme.palette.white};
+  align-items: center;
   gap: 24px;
+  padding: 80px 24px;
+  border: 1px solid ${theme.palette.gray2};
+  border-radius: 8px;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
