05a1eb29848357bbe5d23436962edb767c2b85a4
강나연 <seoultownroad@naver.com>
2024-02-27 12:39:20 +0900

    fix: 재학생 인증 에러 toast

--- src/hooks/auth/useStudentVerification.ts
+++ src/hooks/auth/useStudentVerification.ts
@@ -6,6 +6,7 @@ import useUnivEmail from '@/hooks/zustand/useUnivEmail';
 import RoutePath from '@/routes/routePath';
 import { useForm, FieldValues } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function useStudentVerification() {
   const { univEmail, updateUnivEmail } = useUnivEmail();
@@ -24,12 +25,21 @@ export default function useStudentVerification() {
   };
 
   const onVerifyStudent = async () => {
-    const result = await verifyStudentApi();
+    try {
+      const result = await verifyStudentApi();
 
-    if (result.univStatus === 'VERIFIED') {
-      updateLandingStatue(LandingStatus.Signup);
-      navigation(RoutePath.AuthenticationProcess3_Signup);
-    } else {
+      if (result.univStatus === 'VERIFIED') {
+        updateLandingStatue(LandingStatus.Signup);
+        navigation(RoutePath.AuthenticationProcess3_Signup);
+      } else {
+        // 인증 실패 또는 다른 상태 처리
+        // 여기에서 사용자에게 메시지를 표시하거나 다른 조치를 취할 수 있습니다.
+      }
+    } catch (error: any) {
+      // 에러 처리
+      // 예를 들어, 사용자에게 알림을 표시하거나, 로깅을 수행할 수 있습니다.
+      toast('Failed to verify student:', error);
+      // 에러 상황에 대한 사용자 피드백이나 다른 액션을 추가할 수 있습니다.
     }
   };
 
