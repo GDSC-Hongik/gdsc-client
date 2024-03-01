5cad72f7593417892e885df8d6a7c57601468fab
nimod7890 <nimod1234@g.skku.edu>
2024-03-01 23:49:38 +0900

    feat: landing status(onboarding not opened) 추가

--- src/constants/landingStatus.ts
+++ src/constants/landingStatus.ts
@@ -1,7 +1,8 @@
 const enum LandingStatus {
   StudentAuthentication = 'TO_STUDENT_AUTHENTICATION',
   Signup = 'TO_REGISTRATION',
-  MyPage = 'TO_DASHBOARD'
+  MyPage = 'TO_DASHBOARD',
+  OnboardingNotOpened = 'ONBOARDING_NOT_OPENED'
 }
 
 export default LandingStatus;
