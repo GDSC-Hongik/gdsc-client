0337813b247183584917997822cf50f20c4678f0
hamo-o <yheel369@gmail.com>
2024-07-07 18:42:58 +0900

    feat: 유저정보 API 임시설정

---
+++ src/apis/me/meType.ts
@@ -0,0 +1,10 @@
+export interface MeResponse {
+  memberId: number;
+  studentId: string;
+  name: string;
+  phone: string;
+  department: string;
+  email: string;
+  discordUsername: string;
+  nickname: string;
+}
