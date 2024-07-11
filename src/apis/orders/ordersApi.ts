03e4511cb3d7965ce00e6a686d55a3769f8e5b7a
hamo-o <yheel369@gmail.com>
2024-07-12 00:15:47 +0900

    feat: 임시 주문과 결제 승인 API 구분하기

--- src/apis/orders/ordersApi.ts
+++ src/apis/orders/ordersApi.ts
@@ -2,6 +2,11 @@ import apiClient from '@/apis';
 import { OrderRequest } from './ordersType';
 
 const ordersApi = {
+  POST_PREV_ORDER: async (order: OrderRequest) => {
+    const response = await apiClient.post(`/onboarding/orders`, order);
+    // Todo: error 로직 추가하기
+    return response.data;
+  },
   POST_ORDER: async (order: OrderRequest) => {
     const response = await apiClient.post(`/onboarding/orders`, order);
     // Todo: error 로직 추가하기
