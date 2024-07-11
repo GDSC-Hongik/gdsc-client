f9d9fcc0aa357202cfa0981eb7b0b7aac1d2f260
hamo-o <yheel369@gmail.com>
2024-07-11 23:05:21 +0900

    refactor: 회비 납부 페이지 컴포넌트 분리

--- src/pages/Payments.tsx
+++ src/pages/Payments.tsx
@@ -1,6 +1,6 @@
 import { Flex, Space, Text } from '@/components/common/Wrapper';
 import { media } from '@/styles';
-import Box from 'wowds-ui/Box';
+
 import Button from 'wowds-ui/Button';
 import styled from '@emotion/styled';
 import { color } from 'wowds-tokens';
@@ -10,7 +10,9 @@ import { useNavigate } from 'react-router-dom';
 
 import RoutePath from '@/routes/routePath';
 import { useProduct } from '@/hooks/zustand/useProduct';
-import CouponDropDown from '@/components/payments/CouponDropDown';
+import { CouponDropDown } from '@/components/payments/CouponDropDown';
+import { CalculateBox } from '@/components/payments/CalculateBox';
+import { PaymentItemBox } from '@/components/payments/PaymentItemBox';
 
 export const Payments = () => {
   const navigate = useNavigate();
@@ -27,59 +29,14 @@ export const Payments = () => {
         <Heading typo="h1" color="black">
           회비 납부
         </Heading>
-        <Flex
-          justify="flex-start"
-          direction="column"
-          align="flex-start"
-          gap="sm">
-          <Text typo="h2" color="black">
-            결제 항목
-          </Text>
-          <Box text={name} subText={`금액 ${strAmount}원`} status="success" />
-        </Flex>
-        <Flex
-          justify="flex-start"
-          direction="column"
-          align="flex-start"
-          gap="sm">
-          <Text typo="h2" color="black">
-            할인 쿠폰
-          </Text>
-          <CouponDropDown />
-        </Flex>
+        <PaymentItemBox name={name} amount={strAmount} />
+        <CouponDropDown />
       </Flex>
       <Flex direction="column" gap="lg">
-        <Box
-          status="success"
-          text={
-            <Flex direction="column" gap="sm">
-              <Flex justify="space-between">
-                <Text typo="label2" color="black">
-                  총 회비
-                </Text>
-                <Text typo="body1" color="black">
-                  {strAmount}원
-                </Text>
-              </Flex>
-              <Flex justify="space-between">
-                <Text typo="label2" color="black">
-                  총 할인금액
-                </Text>
-                <Text typo="body1" color="black">
-                  {strDiscount}원
-                </Text>
-              </Flex>
-              <Divider />
-              <Flex justify="space-between">
-                <Text typo="body1" color="black">
-                  총 결제금액
-                </Text>
-                <Text typo="h2" color="black">
-                  {strTotalAmount}원
-                </Text>
-              </Flex>
-            </Flex>
-          }
+        <CalculateBox
+          amount={strAmount}
+          discount={strDiscount}
+          total={strTotalAmount}
         />
         <Button onClick={handleClickRoute}>결제하기</Button>
         <Space height={28} />
@@ -104,11 +61,4 @@ const Wrapper = styled(Flex)`
   ${media.mobile} {
     width: 100vw;
   }
-`;
-
-const Divider = styled.div`
-  width: 100%;
-  height: 1px;
-
-  background: ${color.lightDisabled};
 `;
