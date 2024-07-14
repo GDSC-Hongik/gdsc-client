9102657b9df3e47a72f03550a8e202a42c8c9c6b
SeieunYoo <isho48239@gmail.com>
2024-07-15 01:48:45 +0900

    fix: 하드코딩된 토큰 환경변수에 저장

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,14 +1,15 @@
-import { BASE_URL } from '@/constants/environment';
+import { BASE_URL, DEV_AUTH_TOKEN } from '@/constants/environment';
 import useAuthToken from '@/hooks/auth/useAuthToken';
 import axios from 'axios';
 
-const tokenn = `Bearer eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJnZHNjLWhvbmdpayIsInN1YiI6IjI5NCIsImlhdCI6MTcwOTAzMzUxNywiZXhwIjoxODA5MDQwNzE3LCJyb2xlIjoiUkVHVUxBUiJ9.63RmsXULrgf2Nh0vU4IsJRyeVB_93yo34-U3hTsMm6eTANik5iiBHXBYd86YyY_v`;
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 });
 
-apiClient.defaults.headers.common['Authorization'] = `${tokenn}`;
+apiClient.defaults.headers.common['Authorization'] = DEV_AUTH_TOKEN
+  ? `${DEV_AUTH_TOKEN}`
+  : `Bearer ${useAuthToken().accessToken}`;
 
 export default apiClient;
