9373a7a8e948eb99b5bfac141ebdac1fe6e459c1
hamo-o <yheel369@gmail.com>
2024-07-22 23:59:16 +0900

    chore: 주석 제거

--- src/apis/orders/ordersApi.ts
+++ src/apis/orders/ordersApi.ts
@@ -4,12 +4,10 @@ import { OrderRequest, OrderCompleteRequest } from './ordersType';
 const ordersApi = {
   POST_PREV_ORDER: async (order: OrderRequest) => {
     const response = await apiClient.post(`/onboarding/orders`, order);
-    // Todo: error 로직 추가하기
     return response.data;
   },
   POST_ORDER: async (order: OrderCompleteRequest) => {
     const response = await apiClient.post(`/onboarding/orders/complete`, order);
-    // Todo: error 로직 추가하기
     return response.data;
   }
 };
