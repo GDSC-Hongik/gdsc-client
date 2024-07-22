df087cf7c6a7d2ce2a36d3b9105d1c7b03e095b6
hamo-o <yheel369@gmail.com>
2024-07-22 18:01:45 +0900

    fix: 스토리지 관련 로직 삭제

--- src/hooks/zustand/useProduct.ts
+++ src/hooks/zustand/useProduct.ts
@@ -1,18 +1,4 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage, StateStorage } from 'zustand/middleware';
-import secureLocalStorage from 'react-secure-storage';
-
-const SecureStorage: StateStorage = {
-  getItem: async (name: string): Promise<string | null> => {
-    return secureLocalStorage.getItem(name) as string | null;
-  },
-  setItem: async (name: string, value: string): Promise<void> => {
-    return secureLocalStorage.setItem(name, value);
-  },
-  removeItem: async (name: string): Promise<void> => {
-    return secureLocalStorage.removeItem(name);
-  }
-};
 
 type ProductStore = {
   name: string;
@@ -23,23 +9,15 @@ type ProductStore = {
   setDiscount: (discount: number, couponId: number) => void;
 };
 
-export const useProductStore = create(
-  persist<ProductStore>(
-    (set) => ({
-      name: '2024년 1학기 정회원 회비',
-      amount: 20000,
-      totalAmount: 20000,
-      discount: 0,
-      issuedCouponId: null,
-      setDiscount: (newDiscount, couponId) =>
-        set({ discount: newDiscount, issuedCouponId: couponId })
-    }),
-    {
-      name: 'productStorage',
-      storage: createJSONStorage(() => SecureStorage)
-    }
-  )
-);
+export const useProductStore = create<ProductStore>((set) => ({
+  name: '2024년 1학기 정회원 회비',
+  amount: 20000,
+  totalAmount: 20000,
+  discount: 0,
+  issuedCouponId: null,
+  setDiscount: (newDiscount, couponId) =>
+    set({ discount: newDiscount, issuedCouponId: couponId })
+}));
 
 export const useProduct = () => {
   const { discount, issuedCouponId, setDiscount } = useProductStore(
