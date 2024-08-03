9aec249dc139377c701f051e565ff1b049f8137d
Eugene Kim <eugene02876@gmail.com>
2024-08-04 01:13:14 +0900

    chore: sentry 패키지 기본 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import '@utils/sentry';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
---
+++ src/utils/sentry.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import * as Sentry from '@sentry/react';
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType
+} from 'react-router-dom';
+import { SENTRY_DSN_KEY } from '@/constants/environment';
+
+Sentry.init({
+  dsn: SENTRY_DSN_KEY,
+  integrations: [
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes
+    }),
+    // eslint-disable-next-line import/namespace
+    Sentry.replayIntegration()
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: [
+    /^https:\/\/onboarding.gdschongik.com\/?.*$/,
+    /^https:\/\/api\.gdschongik\.com\/?.*$/
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+});
