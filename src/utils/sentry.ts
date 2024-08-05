69560734f0d5a0a38d7a9f905a1e28bdffee1297
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2024-08-06 00:16:11 +0900

    [Feature]: Sentry 세팅 (#81)
    * chore: sentry 패키지 기본 설치
    
    * feat: Sentry ErrorBoundary 추가
    
    * fix: sentry 관련 기본적인 세팅 완료
    
    * fix: json 문법 수정
    
    * fix: Chatbot 삭제
    
    * fix: 브라우저 설정 환경에 따른 sentry 세팅
    
    * fix: 필요없는 dev 세팅 환경 지우기

---
+++ src/utils/sentry.ts
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import * as Sentry from '@sentry/react';
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType
+} from 'react-router-dom';
+import { SENTRY_DSN_KEY } from '@/constants/environment';
+
+const setSentry = () => {
+  function initSentry() {
+    if (process.env.NODE_ENV !== 'production') return;
+
+    Sentry.init({
+      environment: process.env.VERCEL_ENV,
+      dsn: SENTRY_DSN_KEY,
+      tracesSampleRate: 1.0,
+      integrations: [
+        Sentry.reactRouterV6BrowserTracingIntegration({
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes
+        }),
+        // eslint-disable-next-line import/namespace
+        Sentry.replayIntegration()
+      ],
+
+      tracePropagationTargets: [
+        /^https:\/\/onboarding.gdschongik.com\/?.*$/,
+        /^https:\/\/api\.gdschongik\.com\/?.*$/,
+        /^https:\/\/dev-onboarding.gdschongik.com\/?.*$/,
+        /^https:\/\/dev-api\.gdschongik\.com\/?.*$/
+      ],
+      replaysSessionSampleRate: 0.1,
+      replaysOnErrorSampleRate: 1.0
+    });
+  }
+  return { initSentry };
+};
+
+const sentry = setSentry();
+export default sentry;
