17a8c6d05e2cfa801007bdc8b05a73cec24f74e5
Eugene Kim <eugene02876@gmail.com>
2024-08-07 16:53:42 +0900

    fix: 리다이렉트 정리

--- src/components/auth/guard/AuthAccessGuard.tsx
+++ src/components/auth/guard/AuthAccessGuard.tsx
@@ -1,12 +1,25 @@
 import RoutePath from '@/routes/routePath';
 import { isAuthenticated } from '@/utils/auth';
 import { toast } from 'react-toastify';
-import { Outlet, Navigate } from 'react-router-dom';
+import { useNavigate, Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function AuthAccessGuard() {
-  if (isAuthenticated()) return <Outlet />;
-  else {
-    toast.error('로그인이 필요한 서비스예요.');
-    return <Navigate to={RoutePath.Home} />;
-  }
+  const navigate = useNavigate();
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      toast.error('로그인이 필요한 서비스예요.');
+      setRedirect(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (redirect) {
+      navigate(RoutePath.Home);
+    }
+  }, [redirect, navigate]);
+
+  return isAuthenticated() ? <Outlet /> : null;
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -6,8 +6,6 @@ export * from './MyPageEdit';
 export * from './SignUp';
 export * from './StudentVerification';
 export { default as UpdatedStudentVerification } from './UpdatedStudentVerification';
-export { default as OnboardingNotOpened } from './OnboardingNotOpened';
-export { default as OnboardingClosed } from './OnboardingClosed';
 export * from './redirect/AuthServerRedirectNavigate';
 export * from './redirect/StudentVerificationServerRedirect';
 export * from './PaymentsCheckout';
