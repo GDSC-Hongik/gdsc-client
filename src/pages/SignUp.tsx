0fd9a5d5554c0c7e3c5365e85f574bdd7520cbf2
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2024-08-07 18:14:48 +0900

    [Refactor]: 마이너 UI 이슈 수정 (#90)
    * fix: textfield 가로길이 안맞는 부분 수정
    
    * fix: scroll 반 쯤 내려가 있는 문제 해결

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -188,6 +188,7 @@ export const SignUp = () => {
             render={({ field, fieldState }) => (
               <TextFieldWrapper>
                 <TextField
+                  style={{ minWidth: '100%' }}
                   label="이메일 주소"
                   error={fieldState.invalid}
                   ref={field.ref}
