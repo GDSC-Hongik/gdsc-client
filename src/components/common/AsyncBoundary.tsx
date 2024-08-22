3048efda870dbaba312bc825c06315b09b8d3c46
Eugene Kim <eugene02876@gmail.com>
2024-08-23 02:13:21 +0900

    feat: sentry 에러 잡는 Async component 생성

--- src/components/common/AsyncBoundary.tsx
+++ src/components/common/AsyncBoundary.tsx
@@ -1,3 +1,15 @@
-const AsyncBoundary = () => {};
+import { ErrorBoundary } from '@sentry/react';
+import { Suspense, SuspenseProps } from 'react';
+import LoadingSpinner from './LoadingSpinner';
+
+type AsyncBoundaryProps = Omit<SuspenseProps, 'fallback'>;
+
+const AsyncBoundary = ({ children, ...rest }: AsyncBoundaryProps) => {
+  return (
+    <ErrorBoundary {...rest}>
+      <Suspense fallback={<LoadingSpinner />}>{children}</Suspense>
+    </ErrorBoundary>
+  );
+};
 
 export default AsyncBoundary;
