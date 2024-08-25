af2ad1cf30673e2fc73d2a70eb096f8895ac751c
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2024-08-26 04:30:26 +0900

    [Fix] Sentry 에러 dsn 키 문제 해결 (#107)
    * feat: sentry sourcemap 설정
    
    * fix: sentry 에러바운더리인식문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
-    org: process.env.SENTRY_ORG_NAME,
-    project: process.env.SENTRY_PROJECT_NAME,
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    sourcemaps: {
-      assets: './dist/**',
-      filesToDeleteAfterUpload: '**/*.map'
-    }
-  }), sentryVitePlugin({
-    org: "gdsc-hongik",
-    project: "wow-onboarding"
-  })],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: process.env.VITE_SENTRY_ORG_NAME,
+      project: process.env.VITE_SENTRY_PROJECT_NAME,
+      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        assets: './dist/**',
+        filesToDeleteAfterUpload: '**/*.map'
+      }
+    }),
+    sentryVitePlugin({
+      org: 'gdsc-hongik',
+      project: 'wow-onboarding'
+    })
+  ],
   build: {
     sourcemap: true
   }
