7738ba2f35deeb3fd2c745920d9c9814132a67b9
Eugene Kim <eugene02876@gmail.com>
2024-09-04 23:19:38 +0900

    fix: 정회원 가입 조건 regular일때 보이도록

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -31,6 +31,8 @@ export const Dashboard = () => {
 
   const { member, currentRecruitmentRound, currentMembership } = data;
 
+  console.log(data);
+
   return (
     <div style={{ height: '100%' }}>
       <Wrapper direction="column" justify="flex-start">
@@ -43,9 +45,10 @@ export const Dashboard = () => {
             member={member}
           />
         </Flex>
-        {currentMembership && (
+        {(currentMembership || member.role === 'REGULAR') && (
           <JoinRegularMember
-            paymentStatus={currentMembership.regularRequirement.paymentStatus}
+            member={member}
+            paymentStatus={currentMembership?.regularRequirement.paymentStatus}
           />
         )}
         <AssociateRequirementCheck
