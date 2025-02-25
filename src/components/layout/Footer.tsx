ac3a75b40270ec8d85d43993691a208fe903c1b7
Eugene Kim <67894159+eugene028@users.noreply.github.com>
2025-02-25 23:44:38 +0900

    [QA] Footer 디자인 수정 (#126)
    * fix: gdsc 이름 변경되지 않은 부분 수정
    
    * fix: 0225 QA 수정
    
    * fix: 0225 QA 반영
    
    * fix: footer 디자인 재수정
    
    * fix: footer 디자인 리뉴얼
    
    * fix: 와우 클래스 새로운 도메인으로 연결

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled';
+import { Logo } from '@/assets/LogoIcon';
 import { Link } from 'react-router-dom';
 import { color } from 'wowds-tokens';
 import { Flex, Text } from '../common/Wrapper';
 import { FooterLogoIcon } from '@/assets/FooterLogoIcon';
+import { FooterGithubIcon, FooterInstagramIcon } from '@/assets/FooterIcons';
 
 const Footer = () => {
   return (
     <Container>
-      {/* <Logo /> */}
+      <Logo />
       <FooterLogoIcon />
 
       <Flex direction="column" align="start" gap="md">
@@ -26,13 +28,21 @@ const Footer = () => {
             GDGoC Hongik 가이드라인{' '}
           </Text>
         </Link>
-        <Link to={'https://www.gdschongik.com/term-of-use'}>
-          <Text typo="label2" style={{ textDecoration: 'underline' }}>
-            와우온보딩 이용약관
-          </Text>
-        </Link>
       </Flex>
-
+      <Flex gap="md" align="start" justify="start" style={{ width: '100%' }}>
+        <button
+          onClick={() => {
+            window.location.href = 'https://github.com/GDSC-Hongik';
+          }}>
+          <FooterGithubIcon />
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = 'https://www.instagram.com/gdsc.hongik/';
+          }}>
+          <FooterInstagramIcon />
+        </button>
+      </Flex>
       <Flex direction="column" align="start">
         <Text typo="body3" color="sub">
           와우디벨로퍼스 | 서울특별시 동작구 상도로53길 8, 325동 606호
